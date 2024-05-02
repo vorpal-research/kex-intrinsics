84ae76113466bce7373fd6f8301301c29b2fd817
Azat Abdullin <azat.aam@gmail.com>
2024-05-02 14:14:58 +0200

    optimized assume intrinsics

--- src/main/kotlin/org/vorpal/research/kex/intrinsics/AssertIntrinsics.kt
+++ src/main/kotlin/org/vorpal/research/kex/intrinsics/AssertIntrinsics.kt
@@ -10,6 +10,48 @@ object AssertIntrinsics {
     fun kexAssume(condition: Boolean) {}
 
     /**
+     * Assumes that the value is positive without checking it
+     * */
+    @JvmStatic
+    fun kexAssumePositive(value: Int) {}
+
+    /**
+     * Assumes that the value is positive or zero without checking it
+     * */
+    @JvmStatic
+    fun kexAssumePositiveOrZero(value: Int) {}
+
+    /**
+     * Assumes that the value is positive without checking it
+     * */
+    @JvmStatic
+    fun kexAssumeNegative(value: Int) {}
+
+    /**
+     * Assumes that the value is positive without checking it
+     * */
+    @JvmStatic
+    fun kexAssumeNegativeOrZero(value: Int) {}
+
+    /**
+     * Assumes that the values are equal
+     * */
+    @JvmStatic
+    fun kexAssumeEqual(lhv: Int, rhv: Int) {}
+
+    /**
+     * Assumes that the values are equal
+     * */
+    @JvmStatic
+    fun kexAssumeNotEqual(lhv: Int, rhv: Int) {}
+
+    /**
+     * Assumes that the values are equal
+     * */
+    @JvmStatic
+    fun kexAssumeEqual(lhv: CharArray, rhv: CharArray) {}
+
+    /**
      * Assumes that the given object @param is not null
      * */
     @JvmStatic
